7d13de63d0e70d2576aa59e6fa7cb0b3aef9ff0b
kkilme <jojo5142@naver.com>
2023-05-06 02:58:32 +0900

    index page, local file for cache

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import fetch from "node-fetch"; // node-fetch는 require로 할 수 없음, import 사용
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = 3000;
 
@@ -17,12 +17,12 @@ app.use((req, res, next) => {
 
 app.get("/home", (req, res) => {
   // res.sendFile(__dirname + '/steam.html');
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, "index.html"));
   // res.send("Hello world");
 });
 
 app.get("/", (req, res) => {
-  res.redirect('/home')
+  res.redirect("/home");
   // // res.sendFile(__dirname + '/steam.html');
   // res.send("Hello world!");
   // // res.send("Hello world");
@@ -31,18 +31,22 @@ app.get("/", (req, res) => {
 // 스팀 API 호출
 app.get("/steaminfo", async (req, res) => {
   const steamId = req.query.steamid;
-  console.log('SteamId:'+steamId);
+  console.log("SteamId:" + steamId);
   const apiKey = "CDB6562AD13D438878CDCF95AECC2879";
   try {
-    const steamUserResponse = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`);
+    const steamUserResponse = await fetch(
+      `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`
+    );
     const steamUserData = await steamUserResponse.json();
 
-    const steamGamesResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`);
+    const steamGamesResponse = await fetch(
+      `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`
+    );
     const steamGamesData = await steamGamesResponse.json();
-    
+
     res.json({
       UserData: steamUserData,
-      GameData: steamGamesData
+      GameData: steamGamesData,
     });
 
     // const playtime = steamGamesData.response.games.reduce((total, game) => total + game.playtime_forever, 0);
@@ -71,41 +75,131 @@ app.get("/steaminfo", async (req, res) => {
 
 // })
 
-app.get("/appinfo", async(req,res) => {
+function readJson(filename) {
+  var dt = fs.readFileSync(path.join(__dirname, filename), "utf-8");
+  return JSON.parse(dt);
+}
+
+function writeJson(filename, data) {
+  let obj = JSON.stringify(data);
+  fs.writeFileSync(path.join(__dirname, filename), obj, "utf-8");
+}
+
+function parseData(js, appid) {
+  var parsedjs = {};
+  if (js[appid].success) {
+    var dt = js[appid].data;
+    parsedjs = {
+      steam_appid: dt.steam_appid,
+      success: true,
+      name: dt.name,
+      is_free: dt.is_free,
+      metacritic: dt.metacritic,
+      genres: dt.genres,
+      recommendations: dt.recommendations?.total,
+      achievementnum: dt.achievements?.total,
+      header_image: dt.header_image,
+      price_overview: dt.price_overview,
+    };
+  } else {
+    parsedjs = {
+      success: false,
+    };
+  }
+  return parsedjs;
+}
+app.get("/appinfo", async (req, res) => {
   const appid = req.query.appid;
-  try{
-    //`http://store.steampowered.com/api/appdetails?appids=${appid}&filters=price_overview&format=json`
-    const appdata = await fetch(`http://store.steampowered.com/api/appdetails?appids=${appid}&filters=price_overview`);
-    if(appdata == null){
-      console.log(`Null response: ${appid}`);
+  let appdata = readJson("appdata.json");
+  try {
+    if (!Array.isArray(appid)) {
+      // query.length = 1
+      if (appid in appdata && appdata[appid] != null) {
+        // cache hit
+        console.log(`hit ${appid}`);
+        res.json(appdata[appid]);
+        return;
+      }
+      // cache miss
+      console.log(`miss ${appid}`);
+      const resps = await fetch(
+        `http://store.steampowered.com/api/appdetails?appids=${appid}`
+      );
+      var respsjs = {};
+      try {
+        respsjs = await resps.json();
+        appdata[appid] = parseData(respsjs, appid);
+      } catch (err) {
+        appdata[appid] = null;
+      }
+      writeJson("appdata.json", appdata);
+      res.json(appdata[appid]);
+      return;
     }
-    const appjsondata = await appdata.json();
-    if(appjsondata == null){
-      console.log(`Null response: ${appid}`);
-    }
-    console.log(appjsondata);
-    // console.log(appdata);
-    res.json(appjsondata);
-  } catch(error){
+
+    // var querystrings = []
+    // make fetch urls and store
+    // for(let i = 0; i<appid.length; i++){
+    //   querystrings.push(`http://store.steampowered.com/api/appdetails?appids=${appid[i]}`)
+    // }
+
+    // for (const st of querystrings) {
+    //   console.log(st);
+    // }
+    // console.log(appid);
+    // fetch appinfo of all games asynchronously
+    const resp = await Promise.all(
+      appid.map(async (id) => {
+        if (id in appdata && appdata[id] != null) {
+          // cache hit
+          console.log(`hit ${id}`);
+          return appdata[id];
+        }
+        console.log(`miss ${id}`);
+        const resps = await fetch(
+          `http://store.steampowered.com/api/appdetails?appids=${id}&cc=KR`
+        );
+        let respsjs = "";
+        try {
+          respsjs = await resps.json();
+        } catch (err) {
+          console.log(`Null response: ${id}`);
+          appdata[id] = null;
+          return appdata[id];
+        }
+        if (respsjs == null) {
+          console.log(`js null: ${id}`);
+          appdata[id] = null;
+          return appdata[id];
+        }
+        appdata[id] = parseData(respsjs, id);
+        return appdata[id];
+      })
+    );
+    writeJson("appdata.json", appdata);
+    res.json(resp);
+  } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
-})
+});
 
-app.get("/achivementinfo", async(req,res) => {
+app.get("/achivementinfo", async (req, res) => {
   const appid = req.query.appid;
   const steamid = req.query.steamid;
   const apiKey = "CDB6562AD13D438878CDCF95AECC2879";
-  try{
-    const achievedata = await fetch(`http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${apiKey}&steamid=${steamid}`);
+  try {
+    const achievedata = await fetch(
+      `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${apiKey}&steamid=${steamid}`
+    );
     const achievejsondata = await achievedata.json();
-    console.log(achievejsondata);
+    // console.log(achievejsondata);
     res.json(achievejsondata);
-  } catch(error){
+  } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
-})
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- public/steam.js
+++ public/steam.js
@@ -1,8 +1,8 @@
-const domainname = 'http://localhost:3000';
-var playtimeDict = {}
-var gameDataDict = {}
-var appDataDict = {}
-async function getSteamData(steamId){
+const domainname = "http://localhost:3000";
+var playtimeDict = {};
+var gameDataDict = {};
+var appDataDict = {};
+async function getSteamData(steamId) {
   // steamId = document.getElementById("steamid").value;
   try {
     const response = await fetch(`${domainname}/steaminfo?steamid=${steamId}`);
@@ -20,109 +20,159 @@ async function getSteamData(steamId){
   }
 }
 
-async function loadPage(){
-  var steamId = '76561198818238819';
+function clicktest() {
+  console.log("clicked");
+}
+
+async function loadPage() {
+  var steamId = "76561198818238819";
   const data = await getSteamData(steamId);
-  const GameData = data.GameData.response;
+  const GameData = data.GameData.response.games;
   const UserData = data.UserData.response.players[0];
-
+  console.log(data);
   getPlayTimeData(GameData, UserData);
-  getGameData(GameData);
+  await getGameData(GameData);
 
-  const appId = '374320';
+  const appId = "374320";
   // const testappdata = await (await fetch(`http://localhost:3000/appinfo?appid=${appId}`)).json();
   // console.log(testappdata[appId]);
   // console.log(testappdata);
 
-  const testachievedata = await (await fetch(`${domainname}/achivementinfo?appid=${appId}&steamid=${steamId}`)).json();
+  const testachievedata = await (
+    await fetch(
+      `${domainname}/achivementinfo?appid=${appId}&steamid=${steamId}`
+    )
+  ).json();
   // console.log(testachievedata);
 }
 
-function getPlayTimeData(GameData, UserData){
-  const daysSinceCreated = Math.floor((Date.now() - UserData.timecreated * 1000) / (1000 * 60 * 60 * 24));
-  playtimeDict['totalMinute']= GameData.games.reduce((total, game) => total + game.playtime_forever, 0);
-  playtimeDict['hour'] = (playtimeDict['totalMinute'] / 60).toFixed(1);
-  playtimeDict['oneDayAvg'] = Math.floor(playtimeDict['totalMinute'] / daysSinceCreated);
-  console.log("PlaytimeDict"+playtimeDict);
+function getPlayTimeData(GameData, UserData) {
+  const daysSinceCreated = Math.floor(
+    (Date.now() - UserData.timecreated * 1000) / (1000 * 60 * 60 * 24)
+  );
+  playtimeDict["totalMinute"] = GameData.reduce(
+    (total, game) => total + game.playtime_forever,
+    0
+  );
+  playtimeDict["hour"] = (playtimeDict["totalMinute"] / 60).toFixed(1);
+  playtimeDict["oneDayAvg"] = Math.floor(
+    playtimeDict["totalMinute"] / daysSinceCreated
+  );
+  console.log("PlaytimeDict" + playtimeDict);
 }
 
-async function getGameData(GameData){
-  gameDataDict['OwnedGamesCount'] = GameData.games.length;
-  gameDataDict['neverPlayedGamesCount'] = 0;
-  gameDataDict['TotalPrice'] = await getTotalAppPrice(GameData.games);
-  console.log(GameData.games)
-  var i = 0;
-  GameData.games.forEach(game => {
-    if(game.playtime_forever == '0') gameDataDict['neverPlayedGamesCount']++;
+async function getGameData(GameData) {
+  gameDataDict["OwnedGamesCount"] = GameData.length;
+  gameDataDict["neverPlayedGamesCount"] = 0;
+  await getAppData(GameData);
+  GameData.forEach((game) => {
+    if (game.playtime_forever == "0") gameDataDict["neverPlayedGamesCount"]++;
   });
-  console.log(gameDataDict)
-  console.log(gameDataDict['TotalPrice'])
-  gameDataDict['PlayedGamesCount'] = gameDataDict['OwnedGamesCount'] - gameDataDict['neverPlayedGamesCount'];
+  console.log(gameDataDict);
+  console.log(gameDataDict["TotalInitialPrice"]);
+  gameDataDict["PlayedGamesCount"] =
+    gameDataDict["OwnedGamesCount"] - gameDataDict["neverPlayedGamesCount"];
   // gameDataDict['TotalPrice'] += getAppPrice('374320')
   console.log("GameDataDict" + gameDataDict);
 }
 
-async function getTotalAppPrice(games){
-  const promises = []
-  var querystrings = []
+async function getAppData(games) {
+  var querystrings = [];
   const appNumToQueryAtOnce = 50;
-  gameDataDict['FreeGameCount'] = 0;
-  gameDataDict['UnknownGameCount'] = 0;
+  gameDataDict["FreeGameCount"] = 0;
+  gameDataDict["UnknownGameCount"] = 0;
+  gameDataDict["TotalInitialPrice"] = 0;
 
-  games.forEach(game => {
-    appDataDict[game.appid] = '';
-  })
+  console.log(games);
+  // make keys for appDataDict
+  games.forEach((game) => {
+    appDataDict[game.appid] = "";
+  });
 
-  for(let i = 0, querystring = `${domainname}/appinfo?`; i<games.length; i++){
-    if((i != 0 && i% appNumToQueryAtOnce == 0) || i == games.length-1){
-      querystring += `appid=${games[i].appid}`
+  //#region testcode
+  // test: small num of requests
+  // querystrings.push(`${domainname}/appinfo?appid=374320`);
+  // querystrings.push(`${domainname}/appinfo?appid=57300`);
+  // querystrings.push(`${domainname}/appinfo?appid=391220`);
+
+  // test: not small num of requests
+  // for(let i = 0, querystring = `${domainname}/appinfo?`; i<101; i++){
+  //   if((i != 0 && i% appNumToQueryAtOnce == 0) || i == 100){
+  //     querystring += `appid=${games[i].appid}`
+  //     querystrings.push(querystring);
+  //     querystring = `${domainname}/appinfo?`;
+  //   }
+  //   else {
+  //     querystring += `appid=${games[i].appid}&`;
+  //   }
+  // }
+  //#endregion
+
+  // make fetch urls of all owned games
+  for (
+    let i = 0, querystring = `${domainname}/appinfo?`;
+    i < games.length;
+    i++
+  ) {
+    if ((i != 0 && i % appNumToQueryAtOnce == 0) || i == games.length - 1) {
+      querystring += `appid=${games[i].appid}`;
       querystrings.push(querystring);
       querystring = `${domainname}/appinfo?`;
-    }
-    else {
+    } else {
       querystring += `appid=${games[i].appid}&`;
     }
   }
+  let rawdata = [];
 
-  for (const quer of querystrings) {
-    promises.push(fetch(quer));
+  //#region asynch fetch test
+  // // fetch appinfo of all owned games asynchronously
+  // const rawdata= await Promise.all(querystrings.map(async url => {
+  //     const resp = await fetch(url);
+  //     return resp.json();
+  //   }));
+  // console.log(rawdata);
+  //#endregion
+  // fetch appinfo of all owned games synchronously
+  for (const qstring of querystrings) {
+    const rawres = await fetch(qstring);
+    const jsres = await rawres.json();
+    rawdata.push(jsres);
   }
-  const rest = await Promise.all(promises);
-  promises.length = 0;
-  i = 0
-  games.forEach(game => {
-    i++;
-    if(i>2) return;
-    promises.push(fetch(`${domainname}/appinfo?appid=${game.appid}`))
-  })
-  const res = await Promise.all(promises);
-  promises.length = 0; // clear array
-  res.forEach(r => {
-    promises.push(r.json())
-  })
-  const result = await Promise.all(promises);
-  var pricedict = {}
-  console.log(result);
-  result.forEach(r => { // r[Object.keys(r)[0]]: 특정 앱의 정보 (element: success 여부, price_overview)
-    var data = r[Object.keys(r)[0]]
-    console.log(data)
-    if (!data.success) {
-      console.log(`Failed to get appinfo: ${Object.keys(r)[0]}`);
-      gameDataDict['UnknownGameCount']++;
-      return;
+  console.log(rawdata);
+
+  for (const arr of rawdata) {
+    if (!Array.isArray(arr)) {
+      if (!arr.success) {
+        // failed to get data from steam (maybe deleted game?)
+        console.log(`Failed to get appinfo: ${arr.steam_appid}`);
+        gameDataDict["UnknownGameCount"]++;
+        continue;
+      }
+      if (arr.is_free) {
+        // game is free
+        console.log(`Free game: ${arr.steam_appid}`);
+        gameDataDict["FreeGameCount"]++;
+      } else if (it.price_overview != undefined) {
+        gameDataDict["TotalInitialPrice"] += it.price_overview.initial;
+      }
+      appDataDict[arr.steam_appid] = arr;
+      continue;
     }
-    if (data.data.length == 0) {
-      console.log(`Free game: ${Object.keys(r)[0]}`);
-      gameDataDict['FreeGameCount']++;
-      return;
+    for (const it of arr) {
+      if (!it.success) {
+        console.log(`Failed to get appinfo: ${it}`);
+        gameDataDict["UnknownGameCount"]++;
+        continue;
+      }
+      if (it.is_free) {
+        console.log(`Free game: ${it.steam_appid}`);
+        gameDataDict["FreeGameCount"]++;
+      } else if (it.price_overview != undefined) {
+        gameDataDict["TotalInitialPrice"] += it.price_overview.initial;
+      }
+      appDataDict[it.steam_appid] = it;
     }
-    pricedict[Object.keys(r)[0]] = data.data;
-  })
-  var totalprice = 0;
-  Object.values(pricedict).forEach(data => {
-    console.log(data);
-    totalprice += data.price_overview.final;
-    console.log(data.price_overview.final);
-  })
-  return totalprice;
+  }
+  console.log(appDataDict);
+  console.log(gameDataDict);
 }
